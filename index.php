f534871698e9d21841e2e943d63755e084426e8e
Shaq <tadev3@gmail.com>
2016-04-24 16:55:07 +0000

    Added new favicon and index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-
+	$title = 'phprest by Shaq';
+	echo 'Simple phprest by Shaq<br>';
+	echo 'use POST/GET for<br>';
+	echo '/register.php<br>/login.php<br>/get.php';
 ?>
+<title><?php echo $title; ?></title>
